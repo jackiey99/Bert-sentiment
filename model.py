ac1f718f3502593563dbb6722979d0d736d47c65
Liangyue Li <jackiey99@gmail.com>
2020-10-03 04:42:59 +0000

    define the bert model for sentiment classification

---
+++ model.py
@@ -0,0 +1,16 @@
+import config
+import transformers
+import torch.nn as nn
+
+class BERTBaseUncased(nn.Module):
+    def __init__(self):
+        super(BERTBaseUncased, self).__init__()
+        self.bert = transformers.BertModel.from_pretrained(config.BERT_PATH)
+        self.bert_drop = nn.Dropout(0.3)
+        self.out = nn.Linear(768, 1)
+    
+    def forward(self, ids, mask, token_type_ids):
+        _, x = self.bert(ids, attention_mask=mask, token_type_ids=token_type_ids)
+        x = self.bert_drop(x)
+        output = self.out(x)
+        return output
